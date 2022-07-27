566c1c3ee709657931adea425d1d28a3d36f9471
Юрий Кялов <hikker1998@gmail.com>
2022-07-27 11:52:20 +0300

    New Class BitSet

--- Work_with_Set/Program.cs
+++ Work_with_Set/Program.cs
@@ -18,11 +18,12 @@ namespace Lab2
         //Конструктор с входной строкой
         public void Fill(string str)
         {
-            for (int i = 0; i < str.Length; i++)
+            string[] arrayString = str.Split(',');
+            for (int i = 0; i < arrayString.Length; i++)
             {
-                if (!isHaving(Convert.ToInt16(Convert.ToString(str[i]))))
+                if (!isHaving(Convert.ToInt16(Convert.ToString(arrayString[i]))))
                 {
-                    Add(Convert.ToInt16(Convert.ToString(str[i])));
+                    Add(Convert.ToInt16(Convert.ToString(arrayString[i])));
                 }
             }
         }
@@ -218,9 +219,136 @@ public static SimpleSet operator *(SimpleSet a, SimpleSet b)
 }
     }
 
-    /*class BitSet : Set
+    class BitSet : Set
     {
-    }*/
+        private long value;
+
+        private string arrayShow(short[] array)
+        {
+            string str = "";
+            bool check = false;
+            for (int i = 0; i < array.Length; i++)
+            { 
+                str = array[i] +str;
+            }
+            return str;
+        }
+        //Конвертор Десятичная - двоичная
+        private short[] getByteArray()
+        {
+            short[] byteArray = new short[32];
+            int count = 0;
+            while (value > 0)
+            {
+                byteArray[count] = (short)(value % 2);
+                count++;
+                value /= 2;
+            }
+            return byteArray;
+        }
+        private short[] getByteArray(long target)
+        {
+            short[] byteArray = new short[32];
+            int count = 0;
+            while (target > 0)
+            {
+                byteArray[count] = (short)(target % 2);
+                count++;
+                target /= 2;
+            }
+            return byteArray;
+        }
+        //Конвертор Двоичная - десятичная
+        private long getIntValue(short[] array)
+        {
+            long value=0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                value += array[i] * (long)Math.Pow(2, i);
+            }
+            return value;
+        }
+        //Конструктор с 1 значением
+        public BitSet(int value)
+        {
+            this.value = getIntValue(getByteArray(value));
+        }
+        //Конструктор копирования
+        public BitSet(BitSet a)
+        {
+            this.value = getIntValue(a.getByteArray());
+        }
+        //Конструктор пустого значения
+        public BitSet()
+        {
+            this.value = 0;
+        }
+
+        public override bool isHaving(int item)
+        {
+            if ((item > 32) || (item<1))
+            {
+                return false;
+            }
+            else
+            {
+                if (getByteArray(value)[item - 1] == 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        //Добавление 1 бита
+        public override void Add(int item)
+        {
+            short[] newArray = getByteArray(value);
+            newArray[item - 1] = 1;
+            value = getIntValue(newArray);
+        }
+        //Удаление бита
+        public override void Remove(int item)
+        {
+            short[] newArray = getByteArray(value);
+            newArray[item - 1] = 0;
+            value = getIntValue(newArray);
+        }
+        //Перегрузка вывода
+        public override string ToString()
+        {
+            return "Значение: " + value + "\nЗначение в бит: " + arrayShow(getByteArray(value));
+        }
+        //Перегрузка сложения
+        public static BitSet operator +(BitSet x,BitSet y)
+        {
+            BitSet c = new BitSet();
+            for (int i = 1; i <= 32; i++)
+            {
+                if (x.isHaving(i) || y.isHaving(i))
+                {
+                    c.Add(i);
+                }
+            }
+            return c;
+        }
+        //Перегрузка объединения
+        public static BitSet operator *(BitSet x, BitSet y)
+        {
+            BitSet c = new BitSet();
+            for (int i = 1; i <= 32; i++)
+            {
+                if (x.isHaving(i) & y.isHaving(i))
+                {
+                    c.Add(i);
+                }
+
+            }
+            return c;
+        }
+    }
     class Program
     {
 
@@ -232,28 +360,33 @@ public static SimpleSet operator *(SimpleSet a, SimpleSet b)
         }
         static void Main(string[] args)
         {
-        //Welcome();
-        /*Console.WriteLine("Создаем множество по п.1");
-        SimpleSet test = new SimpleSet(5);
-        Console.WriteLine("Заполняем множество строкой 12345678...");
-        test.Fill("12345678");
-        Console.WriteLine("Дополняем множество заполняя его значениями из массива {22,21,23,1,23}...");
-        int[] testitems = new int[5] {22,21,23,1,23};
-        test.Fill(testitems);
-        Console.WriteLine("Заполняем массив (10:30) случайным числом из интервала...");
-        test.Fill(10, 30);
-        Console.WriteLine("Результат:");
-        Console.WriteLine(test);
-        Console.WriteLine("Максимальное значение множества: "+test.GetmaxValue());
-        Console.WriteLine("Создаем слечайные множества для проверки объединения +");
-        SimpleSet A = new SimpleSet(8);
-        SimpleSet B = new SimpleSet(12);
-        SimpleSet C = new SimpleSet(30);
-        B.Fill("12345");
-        Console.WriteLine("Сложили А:" + A + " и B:" + B + " \nПолучили: " + (A + B));
-        Console.WriteLine("Умножили A:" + A + " и B:" + B + "\nПолучили:" + (A * B));
-        Console.WriteLine("Попробуем с пустым множеством\n Берем А:" + A + " берем С:" + C + " \nПолучаем: "+(A*C));
-        */
+            //Welcome();
+            /*Console.WriteLine("Создаем множество по п.1");
+            SimpleSet test = new SimpleSet(5);
+            Console.WriteLine("Заполняем множество строкой 12345678...");
+            test.Fill("12345678");
+            Console.WriteLine("Дополняем множество заполняя его значениями из массива {22,21,23,1,23}...");
+            int[] testitems = new int[5] {22,21,23,1,23};
+            test.Fill(testitems);
+            Console.WriteLine("Заполняем массив (10:30) случайным числом из интервала...");
+            test.Fill(10, 30);
+            Console.WriteLine("Результат:");
+            Console.WriteLine(test);
+            Console.WriteLine("Максимальное значение множества: "+test.GetmaxValue());
+            Console.WriteLine("Создаем слечайные множества для проверки объединения +");
+            SimpleSet A = new SimpleSet(8);
+            SimpleSet B = new SimpleSet(12);
+            SimpleSet C = new SimpleSet(30);
+            B.Fill("12345");
+            Console.WriteLine("Сложили А:" + A + " и B:" + B + " \nПолучили: " + (A + B));
+            Console.WriteLine("Умножили A:" + A + " и B:" + B + "\nПолучили:" + (A * B));
+            Console.WriteLine("Попробуем с пустым множеством\n Берем А:" + A + " берем С:" + C + " \nПолучаем: "+(A*C));
+            BitSet test = new BitSet(5);
+            test.Fill("1,2,3,4,5,6,7,8,9,10,11,12,13");
+            BitSet A = new BitSet(test);
+            BitSet B = new BitSet(300000);
+            Console.WriteLine("А: "+A+"\nB: "+B+"\nСкладываем: "+(A+B)+"\nОбъединяем: "+(A*B));
+            */
         }
     }
 }
