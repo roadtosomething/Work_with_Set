6ac6d981b94712d0ab4f7b6c9cae36168990b2eb
Юрий Кялов <hikker1998@gmail.com>
2022-07-29 15:37:55 +0300

    User Interface

--- Work_with_Set/Program.cs
+++ Work_with_Set/Program.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 
 namespace Lab2
 {
+    //Исключение выхода за пределы массива 
+    class TargetNotCorrectedValueForSet : Exception
+    {
+        public TargetNotCorrectedValueForSet() :base() 
+        {
+            Console.WriteLine("ВЫход за границы множества");
+        }
+    }
     //Абстрактный класс множества
     abstract class Set
     {
@@ -13,7 +21,7 @@ namespace Lab2
         //Метод для отображения нашего множества
         public override string ToString()
         {
-            return ToString();
+            return "Значения множества: "+ToString();
         }
         //Конструкторы
         //Конструктор с входной строкой
@@ -22,7 +30,14 @@ namespace Lab2
             string[] arrayString = str.Split(',');
             for (int i = 0; i < arrayString.Length; i++)
             {
-                Add(Convert.ToInt16(Convert.ToString(arrayString[i])));
+                try
+                {
+                    Add(Convert.ToInt16(Convert.ToString(arrayString[i])));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(arrayString[i] + "- не число");
+                }
             }
         }
         //Массив на входе
@@ -244,38 +259,32 @@ public static SimpleSet operator *(SimpleSet a, SimpleSet b)
             return value;
         }
         //Конструктор с 1 значением
-        public BitSet(int value)
+        public BitSet(int userValue)
         {
-            this.value = getIntValue(getByteArray(value));
+            this.value = getIntValue(getByteArray(userValue));
         }
         //Конструктор копирования
-        public BitSet(BitSet a)
+        private BitSet(BitSet a)
         {
             this.value = getIntValue(a.getByteArray());
         }
-        //Конструктор пустого значения
-        public BitSet()
+        //Конструктор генерации
+        private BitSet()
         {
             this.value = 0;
         }
 
         public override bool isHaving(int item)
         {
-            if ((item > 32) || (item<1))
+            if (getByteArray(value)[item - 1] == 1)
             {
-                return false;
+                return true;
             }
             else
             {
-                if (getByteArray(value)[item - 1] == 1)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                return false;
             }
+            throw new TargetNotCorrectedValueForSet();
         }
         //Добавление 1 бита
         public override void Add(int item)
@@ -294,7 +303,7 @@ public static SimpleSet operator *(SimpleSet a, SimpleSet b)
         //Перегрузка вывода
         public override string ToString()
         {
-            return "Значение: " + value + "\nЗначение в бит: " + arrayShow(getByteArray(value));
+            return Convert.ToString(value);
         }
         //Перегрузка сложения
         public static BitSet operator +(BitSet x,BitSet y)
@@ -380,10 +389,10 @@ public static SimpleSet operator *(SimpleSet a, SimpleSet b)
                 if (isHaving(i))
                 str += i +",";
             }
-            str = str.Remove(str.Length-1);
             if (str.Length > 0)
             {
-                return "Значения множества: " + str;
+                str = str.Remove(str.Length-1);
+                return str;
             }
             else
             {
@@ -401,42 +410,175 @@ public static SimpleSet operator *(SimpleSet a, SimpleSet b)
         Console.WriteLine("Авторство указано, мой личный блог: https://github.com/roadtosomething");
         Console.WriteLine("Начинаем работу со множествами!");
         }
+        static void Dialog()
+        {
+            bool _process = true;
+            while (_process)
+            {
+                Console.WriteLine("Выберите один из вариантов взаимодействия, в случае выхода из программы введите \"++exit\"");
+                Console.WriteLine("1. Перечисление элементов\n2. Битовый массив\n3. Логический массив");
+                string userWord = Console.ReadLine();
+                if (userWord == "++exit")
+                {
+                    Console.WriteLine("До встречи!");
+                    _process = false;
+                }
+                else
+                {
+                    try
+                    {
+                        int target = Convert.ToInt16(userWord);
+                        switch (target)
+                        {
+                            case 1:
+                                Console.WriteLine("Выбран модуль работы с перечислением элементов");
+                                Console.WriteLine("Ограничения в данном модуле на количество элементов множества нет. \n" +
+                                    "Кадый элемент множества может быть включен в перечисление несколько раз.\n" +
+                                    "Если вы добавили элемент 5 раз в данное множество, то необходимо удалить его равное количество раз\n" +
+                                    "Вывод не учитывает количество одного элемента во множестве");
+                                WorkWithSet(target);
+                                break;
+                            case 2:
+                                Console.WriteLine("Выбран модуль работы с массивом бит");
+                                Console.WriteLine("Ограничения в данном модуле на количество элементов множества:\n" +
+                                    "Элемент должен соответствовать условия принадлежности отрезку [1;32] (так как работа ведется с битовым массивом данных). \n" +
+                                    "Кадый элемент множества может быть включен в перечисление несколько раз.\n" +
+                                    "Вывод показывает число в десятичной системе и битовое представление");
+                                WorkWithSet(target);
+                                break;
+                            case 3:
+                                Console.WriteLine("Выбран модуль работы с логическим массивом");
+                                Console.WriteLine("Ограничения в данном модуле на количество элементов множества нет. \n" +
+                                    "Каждый элемент имеет свойство есть или нет во множестве.\n" +
+                                    "Вывод показывает на экран множество элементов принадлежащих данному");
+                                WorkWithSet(target);
+                                break;
+                            default:
+                                Console.WriteLine("Неизвестная команда");
+                                break;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Неизвестная команда. Повторите попытку...");
+                    }
+                }
+            }
+        }
+        static void WorkWithSet(int setNum)
+        {
+            //Дублирвоание взаимодействия для разных методов --------------------------------------------------------------------
+            switch (setNum)
+            {
+                case 1:
+                    Console.WriteLine("Создайте множество указав его максимальный элемент. Элемент должен быть натуральным числом.");
+                    try
+                    { 
+                        int target = Convert.ToInt16(Console.ReadLine());
+                        MultiSet set = new MultiSet(target);
+                        Console.WriteLine("Множество создано. Значения множества: "+set);
+                        MultiSetWorking(set);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Введите число!");
+                        WorkWithSet(setNum);
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Создайте множество указав его максимальный элемент. Элемент должен быть натуральным числом." +
+                        "\nНЕ ПРЕВЫШАТЬ 32, так как работа с битовым массивом.");
+                    try
+                    {
+                        int target = Convert.ToInt16(Console.ReadLine());
+                        BitSet set = new BitSet(target);
+                        Console.WriteLine("Множество создано. Значения множества: " + set);
+                        MultiSetWorking(set);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Введите число!");
+                        WorkWithSet(setNum);
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("Создайте множество указав его максимальный элемент. Элемент должен быть натуральным числом.");
+                    try
+                    {
+                        int target = Convert.ToInt16(Console.ReadLine());
+                        SimpleSet set = new SimpleSet(target);
+                        Console.WriteLine("Множество создано. Значения множества: " + set);
+                        MultiSetWorking(set);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Введите число!");
+                        WorkWithSet(setNum);
+                    }
+                    break;
+            }
+        }
+        static void MultiSetWorking(Set set)
+        {
+            Console.WriteLine("Выберите метод взаимодействия с множеством:\n" +
+                "1. Добавить элемент\n" +
+                "2. Удалить элемент\n" +
+                "3. Заполнить строкой\n" +
+                "4. Заполнить из массива (из файла)\n" +
+                "5. Заполнить случайным числом из диапазона (min,max)\n" +
+                "6. Просмотреть значения множества" +
+                "\nДля возврата введите ++back");
+            bool _working = true;
+            while (_working)
+            {
+                Console.Write("Какую команду выполнить? ");
+                string userWord = Console.ReadLine();
+                int item;
+                switch (userWord)
+                {
+                    case "1":
+                        Console.WriteLine("Введите элемент: ");
+                        item = Convert.ToInt32(Console.ReadLine());
+                        set.Add(item);
+                        break;
+                    case "2":
+                        Console.WriteLine("Введите элемент: ");
+                        item = Convert.ToInt32(Console.ReadLine());
+                        set.Remove(item);
+                        break;
+                    case "3":
+                        Console.WriteLine("Введите строку перечилсяйте элементы через \",\"");
+                        set.Fill(Console.ReadLine());
+                        break;
+                    case "4":
+                        //
+                        break;
+                    case "5":
+                        Console.WriteLine("Введите значения максимального и минимального значения интервала min max\n");
+                        Console.Write("min=");
+                        int min = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("max=");
+                        int max = Convert.ToInt32(Console.ReadLine());
+                        set.Fill(min, max);
+                        break;
+                    case "6":
+                        Console.WriteLine(set);
+                        break;
+                    case "++back":
+                        Console.WriteLine("Возврат к меню");
+                        _working = false;
+                        break;
+                    default:
+                        Console.WriteLine("Неизвестная команда, попробуйте еще раз...");
+                        MultiSetWorking(set);
+                        break;
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            //Welcome();
-            /*Console.WriteLine("Создаем множество по п.1");
-            SimpleSet test = new SimpleSet(5);
-            Console.WriteLine("Заполняем множество строкой 12345678...");
-            test.Fill("12345678");
-            Console.WriteLine("Дополняем множество заполняя его значениями из массива {22,21,23,1,23}...");
-            int[] testitems = new int[5] {22,21,23,1,23};
-            test.Fill(testitems);
-            Console.WriteLine("Заполняем массив (10:30) случайным числом из интервала...");
-            test.Fill(10, 30);
-            Console.WriteLine("Результат:");
-            Console.WriteLine(test);
-            Console.WriteLine("Максимальное значение множества: "+test.GetmaxValue());
-            Console.WriteLine("Создаем слечайные множества для проверки объединения +");
-            SimpleSet A = new SimpleSet(8);
-            SimpleSet B = new SimpleSet(12);
-            SimpleSet C = new SimpleSet(30);
-            B.Fill("12345");
-            Console.WriteLine("Сложили А:" + A + " и B:" + B + " \nПолучили: " + (A + B));
-            Console.WriteLine("Умножили A:" + A + " и B:" + B + "\nПолучили:" + (A * B));
-            Console.WriteLine("Попробуем с пустым множеством\n Берем А:" + A + " берем С:" + C + " \nПолучаем: "+(A*C));
-            BitSet test = new BitSet(5);
-            test.Fill("1,2,3,4,5,6,7,8,9,10,11,12,13");
-            BitSet A = new BitSet(test);
-            BitSet B = new BitSet(300000);
-            Console.WriteLine("А: "+A+"\nB: "+B+"\nСкладываем: "+(A+B)+"\nОбъединяем: "+(A*B));
-            MultiSet test = new MultiSet(5);
-            Console.WriteLine(test);
-            test.Fill("1,2,8,10,101,101,23,32,44");
-            test.Remove(101);
-            Console.WriteLine(test);
-            test.Fill(25, 30);
-            Console.WriteLine(test);
-            */
+            Welcome();
+            Dialog();
         }
     }
 }
